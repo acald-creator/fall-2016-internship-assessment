92ca56efdc125ed37a8fd7f221aa8c6db7d70f96
Antonette Caldwell <antonette.caldwell@outlook.com>
2020-12-05 19:25:55 -0600

    Add HelloWorld to test

--- svelte-components/src/HelloWorld.svelte
+++ svelte-components/src/HelloWorld.svelte
@@ -1,1 +1,10 @@
-<p>Hello World!</p>
+<h1>Hello World</h1>
+
+<style>
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+</style>
